416750516da9ecd9033d70f6c9bc7315adf4b9ea
Aurélie ANAIS <aurelie.aanais@gmail.com>
2021-10-25 15:45:44 +0200

    update .bat

--- Script.bat
+++ Script.bat
@@ -1,2 +1,3 @@
 @echo off
-cmd /k "cd /d C:\Users\aurel\OneDrive\_WORK0\IngenieurIA\projets\Projet1MetierIA\OP_Projet1\env\Scripts & activate & cd /d C:\Users\aurel\OneDrive\_WORK0\IngenieurIA\projets\Projet1MetierIA\OP_Projet1"
+set currentpath=%~dp0
+cmd /k "cd /d currentpath & cd /d env\Scripts & activate & cd /d %cd%"
